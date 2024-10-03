56fe5aab515668ad96b16ab2a741a49e72793dac
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2024-10-03 23:59:11 +0100

    uses has instead of full url

--- tests/learn_more.spec.js
+++ tests/learn_more.spec.js
@@ -1,8 +1,7 @@
 const {test, expect} = require("@playwright/test");
 
 test('pressing learn more button takes you to the learn more page', async ({ page }) => {
-  const normaliseURL = (url) => new URL(url).pathname.replace('//', '/')
   await page.goto('index.html');
   await page.click('a:has-text("Learn More")');
-  await expect(normaliseURL(page.url())).toBe(normaliseURL(`file:///${process.cwd()}/index.html#features`));
+  await expect(new URL(page.url()).hash).toBe('#features');
 });
