775c8bbae42a6a8d47b3e19e4948625074ce1f05
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2024-10-03 23:56:02 +0100

    normalised url and removed double forward slash

--- tests/learn_more.spec.js
+++ tests/learn_more.spec.js
@@ -1,8 +1,8 @@
 const {test, expect} = require("@playwright/test");
 
 test('pressing learn more button takes you to the learn more page', async ({ page }) => {
-  const normaliseURL = (url) => new URL(url)
+  const normaliseURL = (url) => new URL(url).pathname.replace('//', '/')
   await page.goto('index.html');
   await page.click('a:has-text("Learn More")');
-  await expect(normaliseURL(page.url()).pathname).toBe(normaliseURL(`file:///${process.cwd()}/index.html#features`).pathname);
+  await expect(normaliseURL(page.url())).toBe(normaliseURL(`file:///${process.cwd()}/index.html#features`));
 });
