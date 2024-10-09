bcfac08f0f8026b5ec5234908c245244e9a9d761
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2024-10-09 21:11:44 +0100

    changed the mobile phone os's to come first in if statement, as both linux and android can occur, but we want to prioritize android

--- static/js/index.js
+++ static/js/index.js
@@ -68,7 +68,13 @@ function osSpecificDownloadButton() {
 
 function getOS() {
     const userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.includes('windows')) {
+    if (userAgent.includes('android')) {
+        return "android";
+    }
+    else if (userAgent.includes("iphone") || userAgent.includes("ipad")) {
+        return "ios";
+    }
+    else if (userAgent.includes('windows')) {
         return 'windows';
     }
     else if (userAgent.includes('mac os')) {
@@ -76,12 +82,6 @@ function getOS() {
     }
     else if (userAgent.includes('linux')) {
         return 'linux'
-    }
-    else if (userAgent.includes('android')) {
-        return "android";
-    }
-    else if (userAgent.includes("iphone") || userAgent.includes("ipad")) {
-        return "ios";
     }
     else {
         return 'unknown'
