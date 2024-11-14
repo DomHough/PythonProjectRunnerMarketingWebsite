5abf2e1623e1fe5bd42f02d50ddc635e2998355a
DOMINIC ROLLASON-HOUGH <domhough@hotmail.co.uk>
2024-11-14 00:55:25 +0000

    exported values

--- static/js/consts.js
+++ static/js/consts.js
@@ -1,3 +1,3 @@
-const bucketName = 'pyrun-application-repository'
-const region = 'eu-west-2'
-const s3Url = `http://${bucketName}.s3.${region}.amazonaws.com`
+export const bucketName = 'pyrun-application-repository'
+export const region = 'eu-west-2'
+export const s3Url = `http://${bucketName}.s3.${region}.amazonaws.com`
